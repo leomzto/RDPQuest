9f1f2950044e4c9d05732f95d2d48ce38b64946e
Manzato <leopmanzato@gmail.com>
2025-05-22 04:55:21 -0300

    rdpquest

---
+++ RDPQuest/headers/consts.h
@@ -0,0 +1,10 @@
+#ifndef CONSTS_H
+#define CONSTS_H
+
+#include "defines.h"
+#include "estruturas.h"
+
+extern const Classe CLASSES[NUM_CLASSES];
+extern const Inimigo INIMIGOS[NUM_INIMIGOS];
+
+#endif
---
+++ RDPQuest/headers/includes.h
@@ -0,0 +1,18 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "defines.h"
+#include "consts.h"
+#include "estruturas.h"
+#include "utilidades.h"
+#include "funcoes.h"
+
+#endif
