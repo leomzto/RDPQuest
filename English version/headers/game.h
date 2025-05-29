c9a77ba2012f56c0c2911020e082d50a610fd7e4
Leo Manzato <leopmanzato@gmail.com>
2025-05-29 02:20:35 -0300

    English Version - v1.0.4

--- English version/headers/game.h
+++
@@ -1,9 +0,0 @@
-#ifndef GAME_H
-#define GAME_H
-
-#include "player.h"
-
-int gameMenu(void);
-void startGame(Player player);
-
-#endif
--- English version/headers/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-void clearBuffer(void);
-void loadingScreen(int time, char c, int pos_waiting);
-void clearScreen(int waiting);
-
-#endif
